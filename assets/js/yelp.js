0dbe8eae2dc1988f96832771bea1a022e6141fdf
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-10 20:01:11 -0800

    pull search form elements to js file

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,10 +1,16 @@
+// grab html elements from the DOM
 var yelpContainer = document.getElementById("results");
 var searchHistoryCont = document.getElementById("searches");
+var submitBtn = documnet.getElementById("submit-search");
+var businessInput = document.getElementById("business");
 
 var apiUrl = "https://api.yelp.com/v3/businesses/search";
 
 // function to get yelp api data
 var getYelp = function() {
+    // set business variable to business input value
+    var business = $("#business").val();
+    
     fetch(apiUrl).then(function(response) {
         // check to see if response is successful
         if (response.ok) {
