515454054459233bac1452630abea0845ba7de16
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-10 19:49:23 -0800

    add a function to retrieve yelp api

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,2 +1,20 @@
 var yelpContainer = document.getElementById("results");
 var searchHistoryCont = document.getElementById("searches");
+
+var apiUrl = "https://api.yelp.com/v3/businesses/search";
+
+// function to get yelp api data
+var getYelp = function() {
+    fetch(apiUrl).then(function(response) {
+        // check to see if response is successful
+        if (response.ok) {
+            return response.json().then(function(data) {
+                // console log data
+                console.log(data);
+            });
+        } else {
+            // if response is unsuccessful, alert user
+            alert("Error: " + response.statusText);
+        };
+    });
+};
